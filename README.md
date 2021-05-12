f77685c120ca65e4eae2f271517ad4ac3d5060ea
Angus Russell <angus.russell89@gmail.com>
2021-05-12 13:38:10 +1000

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# react-render-if-visible
+# React RenderIfVisible
 Harness the power of Intersection Observers for simple list virtualization in React
+
+This tiny React component is a drop-in list virtualization alternative that defers rendering of its children until it is on or near the screen. Unlike other virtualization libraries, it takes about one minute to integrate and doesn't require you to change your code other than wrapping each item in `<RenderIfVisible></RenderIfVisible>`.
+
+It works equally well with responsive item heights, responsive grids, non-flat lists, and any other HTML that would normally make other virtualization libraries complicated to implement.
+
+This solution has been used successfully in production on [NightCafe Creator](https://creator.nightcafe.studio) for 9+ months.
+
+## Install and integrate in 1 minute
+
+First, install the component from npm.
+
+```bash
+npm install react-render-if-visible --save
+```
+
+Then, import the component and wrap each child with it.
+
+```javascript
+import React from 'react'
+import RenderIfVisible from 'react-render-if-visible'
+import MyListItem from './list-item' 
+
+const ESTIMATED_ITEM_HEIGHT = 200
+
+export const MyItemList = (items) => (
+  <div className="my-list">
+    {items.map(item => (
+      <RenderIfVisible defaultHeight={ESTIMATED_ITEM_HEIGHT}>
+        <MyListItem item={item} />
+      </RenderIfVisible>
+    ))}
+  </div>
+)
+```
+
+## Props
+
+- `defaultHeight?: number` __Default: 300__ An estimate of the element's height.
+- `visibleOffset?: number` __Default: 1000__ How far outside the viewport in pixels should elements be considered visible?
+- `root?: HTMLElement` __Default: null__ [Root element](https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#intersection_observer_concepts_and_usage) passed to `IntersectionObserver`.
+- `children: React.ReactNode` The component(s)/element(s) for which to defer rendering.
---
+++ src/render-if-visible.tsx
@@ -0,0 +1,70 @@
+import React, { useState, useRef, useEffect } from 'react'
+
+const isServer = typeof window === 'undefined'
+
+type Props = {
+  /** An estimate of the element's height */
+  defaultHeight?: number
+  /** How far outside the viewport in pixels should elements be considered visible?  */
+  visibleOffset?: number
+  root?: HTMLElement | null
+  children: React.ReactNode
+}
+
+const RenderIfVisible = ({
+  defaultHeight = 300,
+  visibleOffset = 1000,
+  root = null,
+  children
+}: Props) => {
+  const [isVisible, setIsVisible] = useState<boolean>(isServer)
+  const placeholderHeight = useRef<number>(defaultHeight)
+  const intersectionRef = useRef<HTMLDivElement>(null)
+
+  // Set visibility with intersection observer
+  useEffect(() => {
+    if (intersectionRef.current) {
+      const observer = new IntersectionObserver(
+        entries => {
+          if (typeof window !== undefined && window.requestIdleCallback) {
+            window.requestIdleCallback(
+              () => setIsVisible(entries[0].isIntersecting),
+              {
+                timeout: 600
+              }
+            )
+          } else {
+            setIsVisible(entries[0].isIntersecting)
+          }
+        },
+        { root, rootMargin: `${visibleOffset}px 0px ${visibleOffset}px 0px` }
+      )
+      observer.observe(intersectionRef.current)
+      return () => {
+        if (intersectionRef.current) {
+          observer.unobserve(intersectionRef.current)
+        }
+      }
+    }
+    return () => {}
+  }, [intersectionRef])
+
+  // Set true height for placeholder element after render.
+  useEffect(() => {
+    if (intersectionRef.current && isVisible) {
+      placeholderHeight.current = intersectionRef.current.offsetHeight
+    }
+  }, [isVisible, intersectionRef])
+
+  return (
+    <div ref={intersectionRef}>
+      {isVisible ? (
+        <>{children}</>
+      ) : (
+        <div style={{ height: placeholderHeight.current }} />
+      )}
+    </div>
+  )
+}
+
+export default RenderIfVisible
